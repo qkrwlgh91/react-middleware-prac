e4fa31e3851a934be8d5c92c5c071b202458b035
qkrwlgh91 <qkrwlgh91@gmail.com>
2020-11-16 17:54:35 +0900

    post function

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import CounterContainer from './containers/CounterContainer';
 import PostListContainer from './containers/PostListContainer';
+import { Route } from 'react-router-dom';
+import PostListPage from './pages/PostListPage';
+import PostPage from './pages/PostPage';
 
 function App() {
   return (
     <>
       <CounterContainer />
-      <PostListContainer />
+      <Route path="/" component={PostListPage} exact={true} />
+      <Route path="/:id" component={PostPage} />
     </>
   )
 }
