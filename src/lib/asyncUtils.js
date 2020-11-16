3915d795610b18d5c25eacac26f767fc79222fca
qkrwlgh91 <qkrwlgh91@gmail.com>
2020-11-16 11:11:52 +0900

    redux thunk promise2 && added lib

---
+++ src/lib/asyncUtils.js
@@ -0,0 +1,51 @@
+// Promise에 기반한 Thunk를 만들어 주는 함수
+export const createPromiseThunk = (type, promiseCreator) => {
+    const [SUCCESS, ERROR] = [`${type}_SUCCESS`, `${type}_ERROR`];
+
+    // 이 함수는 promiseCreator가 단 하나의 파라미터만 받는다는 전제하에 작성되었음
+    // 만약 여러 종류의 파라미터를 전달해야하는 상황에서는 객체 타입의 파라미터를 받아오도록 하면 됨
+    // 예: writeComment({postId: 1, text: '댓글내용' });
+
+    return param => async dispatch => {
+        // 요청 시작
+        dispatch({ type, param });
+        try {
+            // 결고물의 이름을 payload라는 이름으로 통일시킴
+            const payload = await promiseCreator(param);
+            dispatch({ type: SUCCESS, payload }); //성공
+        } catch (e) {
+            dispatch({ type: ERROR, payload: e, error: true }); // 실패
+        }
+    }
+
+};
+
+// 리듀서에서 사용 할 수 있는 여러 유틸 함수
+export const reducerUtils = {
+    // 초기 상태. 초기 data 값은 기본적으로 null이지만 바꿀 수 있음
+    initial: (initialData = null) => ({
+        loading: false,
+        data: initialData,
+        error: null
+    }),
+    // 로딩중 상태. prevState의 경우엔 기본값은 nulldlwlaks
+    // 따로 값을 지정하면 null로 바꾸지 않고 다른 값을 유지시킬 수 있음
+    loading: (prevState = null) => ({
+        loading: true,
+        data: prevState,
+        error: null
+    }),
+    // 성공 상태
+    success: payload => ({
+        loading: false,
+        data: payload,
+        error: null
+    }),
+    // 실패 상태
+    error: error => ({
+        loading: false,
+        data: null,
+        error: error
+    })
+
+}
--- src/modules/posts.jsx
+++ src/modules/posts.jsx
@@ -43,3 +43,76 @@ export const getPost = id => async dispatch => {
     }
 };
 
+const initialState = {
+    posts: {
+        loading: false,
+        data: null,
+        error: null
+    },
+    post: {
+        loading: false,
+        data: null,
+        error: null
+    }
+};
+
+export default function posts(state = initialState, action) {
+    switch (action.type) {
+        case GET_POSTS:
+            return {
+                ...state,
+                posts: {
+                    loading: true,
+                    data: null,
+                    error: null
+                }
+            }
+        case GET_POSTS_SUCCESS:
+            return {
+                ...state,
+                posts: {
+                    loading: true,
+                    data: action.posts,
+                    error: null
+                }
+            }
+        case GET_POSTS_ERROR:
+            return {
+                ...state,
+                posts: {
+                    loading: true,
+                    data: null,
+                    error: action.error
+                }
+            }
+        case GET_POST:
+            return {
+                ...state,
+                post: {
+                    loading: true,
+                    data: null,
+                    error: null
+                }
+            }
+            case GET_POST_SUCCESS:
+                return {
+                    ...state,
+                    post: {
+                        loading: true,
+                        data: action.post,
+                        error: null
+                    }
+                };
+            case GET_POST_ERROR:
+                return {
+                    ...state,
+                    post: {
+                        loading: true,
+                        data: null,
+                        error: action.error
+                    }
+                };
+            default:
+                return state;
+    }
+}
