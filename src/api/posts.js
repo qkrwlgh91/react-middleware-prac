462d92bdff1d0652a3c457c1738e646afbf4e81d
qkrwlgh91 <qkrwlgh91@gmail.com>
2020-11-19 16:47:45 +0900

    proxy applied

--- src/api/posts.js
+++ src/api/posts.js
@@ -39,11 +39,11 @@ export const getPostById = async id => {
 import axios from 'axios';
 
 export const getPosts = async () => {
-    const response = await axios.get('http://localhost:4000/posts');
+    const response = await axios.get('/posts');
     return response.data;
 }
 
 export const getPostById = async id => {
-    const response = await axios.get(`http://localhost:4000/posts/${id}`);
+    const response = await axios.get(`/posts/${id}`);
     return response.data;
 }
