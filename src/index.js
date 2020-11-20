8bab978ff17e3c816d52767c6553391f2d9b0050
qkrwlgh91 <qkrwlgh91@gmail.com>
2020-11-20 15:01:41 +0900

    redux-saga counter practice ( generator, delay, put, takeEvery, takeLatest)

--- src/index.js
+++ src/index.js
@@ -5,14 +5,16 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
-import rootReducer from './modules';
+import rootReducer, { rootSaga } from './modules';
 import logger from 'redux-logger';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import ReduxThunk from 'redux-thunk';
 import { Router } from 'react-router-dom';
 import { createBrowserHistory } from 'history';
+import createSagaMiddleware from 'redux-saga';
 
 const customHistory =createBrowserHistory();
+const sagaMiddleware = createSagaMiddleware(); // 사가 미들웨어를 만듭니다.
 
 const store = createStore(
   rootReducer,
@@ -20,12 +22,15 @@ const store = createStore(
   composeWithDevTools(
     applyMiddleware(
       // redux-thunk의 withExtraArgument를 사용하면 thunk함수에서 사전에 정해준 값들을 참조할 수 있다.
-      ReduxThunk.withExtraArgument({ history: customHistory }), 
+      ReduxThunk.withExtraArgument({ history: customHistory }),
+      sagaMiddleware, // 사가 미들웨어 적용
       logger
       )
     )
   ); // 여러개의 미들웨어를 적용할 수 있음
 
+sagaMiddleware.run(rootSaga); // 루트 사가를 실행해줌
+// 주의: 스토어 생성이 된 다음에 위 코드를 실행해야 한다
 
 ReactDOM.render(
   <Router history={customHistory}>
